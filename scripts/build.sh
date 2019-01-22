24bbc2cee53d2429c1a32188a56a31dd20217667
shiv-rajawat <bindassmangal@gmail.com>
2019-01-22 23:03:26 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ terraform init
 
 terraform apply -var "access_key=$AWS_ACCESS_KEY_ID" -var "secret_key=$AWS_SECRET_ACCESS_KEY" -var-file=param.tfvars -auto-approve
 ## destroying
-terraform destroy -var "access_key=$AWS_ACCESS_KEY_ID" -var "secret_key=$AWS_SECRET_ACCESS_KEY" -var-file=param.tfvars -auto-approve
+terraform destroy -var "access_key=$AWS_ACCESS_KEY_ID" -var "secret_key=$AWS_SECRET_ACCESS_KEY" -var-file=param.tfvars -yes
