ab3dede83e96f4b71be7c128f88405452b8cd657
shiv-rajawat <bindassmangal@gmail.com>
2019-01-22 20:20:01 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo $PWD
 cd $PWD/tf-code
+echo $PWD
+echo $HOME
 terraform init -backend-config="region=us-east-2"
 terraform apply -var "access_key=$AWS_ACCESS_KEY_ID" -var "secret_key=$AWS_SECRET_ACCESS_KEY" -var-file=param.tfvars -auto-approve
